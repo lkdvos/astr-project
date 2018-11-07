5d957743274d532f5acd16588fd434212bf065e8
DevosLukas <Lukas.Devos@uGent.be>
2018-11-07 15:42:02 +0100

    Update vector to include a lot more data types and operations

--- program/vector.cpp
+++ program/vector.cpp
@@ -1,3 +1,9 @@
+//==============================================================================
+//
+//.cpp file for all necessary data types for project
+//
+//==============================================================================
+
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <fstream>
@@ -6,6 +12,11 @@
 #include "vector.h" // include header
 using namespace std;
 
+//==============================================================================
+//
+//implement class for 3D vectors with operators and assignment operators
+//
+//==============================================================================
 
 double Vec::r() const { return sqrt(_x*_x + _y * _y + _z * _z); }
 double Vec::r2() const { return _x * _x + _y * _y + _z * _z; }
@@ -64,19 +75,198 @@ void print(Vec a)
 	cout << '(' << a.x() << ',' << a.y() << ',' << a.z() << ')' << endl;
 }
 
+//==============================================================================
+
 void print(double s)
 {
 	cout << s << endl;
 }
 
-int main() {
+//==============================================================================
+//
+//implement class for 6D vectors containing position and velocity
+//
+//==============================================================================
 
-	double s = 2;
-	Vec a(1,2,3);
-	Vec b(3,4,5);
-	print(a*b);
-	print(s*a);
-	print(a / s);
-	print(a.r());
-	print(a.mult(b));
+//getters
+Vec phaseVec::pos() const { return _x; }
+Vec phaseVec::vel() const { return _v; }
+
+
+//assignment operators
+phaseVec& phaseVec::operator+=(phaseVec v) {
+	_x += v._x;
+	_v += v._v;
+	return *this;
+}
+
+phaseVec& phaseVec::operator-=(phaseVec v) {
+	_x -= v._x;
+	_v -= v._v;
+	return *this;
+}
+
+phaseVec& phaseVec::operator*=(double s) {
+	_x *= s;
+	_v *= s;
+	return *this;
+}
+
+phaseVec& phaseVec::operator/=(double s) {
+	_x /= s;
+	_v /= s;
+	return *this;
+}
+
+//operators
+phaseVec operator+(phaseVec a, phaseVec b) { return a += b; }
+phaseVec operator-(phaseVec a, phaseVec b) { return a -= b; }
+
+//==============================================================================
+//
+//implement class for a body in the system
+//
+//==============================================================================
+
+//constructor
+Body::Body(double x, double y, double z, double v_x, double v_y, double v_z, double m) {
+	_x = Vec(x, y, z);
+	_v = Vec(v_x, v_y, v_z);
+	_m = m;
+}
+
+//body/phaseVec operators
+Body& Body::operator+=(phaseVec b) {
+	_x += b.pos();
+	_v += b.vel();
+	return *this;
+}
+
+Body& Body::operator-=(phaseVec b) {
+	_x -= b.pos();
+	_v -= b.vel();
+	return *this;
+}
+
+Body& Body::operator*=(double s) {
+	_x *= s;
+	_v *= s;
+	return *this;
+}
+
+Body& Body::operator/=(double s) {
+	_x /= s;
+	_v /= s;
+	return *this;
+}
+
+//==============================================================================
+//
+//create operations for vector<Body> adding/multiplication/etc
+//
+//==============================================================================
+
+vector<Body> operator+(vector<Body> a,vector<phaseVec> b) {
+	//todo: add check for size of vectors...
+	vector<Body> output(a.size());
+	for (size_t i=0; i!=a.size(); ++i) {
+		output.push_back(a[i] += b[i]);
+	}
+	return output;
+}
+
+vector<Body> operator-(vector<Body> a,vector<phaseVec> b) {
+	//todo: add check for size of vectors...
+	vector<Body> output(a.size());
+	for (size_t i=0; i!=a.size(); ++i) {
+		output.push_back(a[i] -= b[i]);
+	}
+	return output;
+}
+
+vector<phaseVec> operator/(vector<phaseVec> a, double s) {
+	//todo: add check for size of vectors...
+	vector<phaseVec> output(a.size());
+	for (size_t i=0; i!=a.size(); ++i) {
+		output.push_back(a[i] /= s);
+	}
+	return output;
+}
+
+vector<phaseVec> operator*(vector<phaseVec> a, double s) {
+	//todo: add check for size of vectors...
+	vector<phaseVec> output(a.size());
+	for (size_t i=0; i!=a.size(); ++i) {
+		output.push_back(a[i] *= s);
+	}
+	return output;
+}
+
+vector<phaseVec> operator*(double s, vector<phaseVec> a) {
+	//todo: add check for size of vectors...
+	vector<phaseVec> output(a.size());
+	for (size_t i=0; i!=a.size(); ++i) {
+		output.push_back(a[i] *= s);
+	}
+	return output;
+}
+
+//==============================================================================
+//
+//implement class for a constellation of (variable) N bodys
+//
+//==============================================================================
+
+//constructor
+Constellation::Constellation() {
+	double _t = 0;
+	vector<Body> _y;
+	double _E = calcE();
+}
+
+Constellation::Constellation(vector<Body> y) {
+	double _t = 0;
+	vector<Body> _y = y;
+	double _E = calcE();
+}
+
+//getters
+Body Constellation::particle(size_t n) {
+	return _y[n];
+}
+
+double Constellation::time() {
+	return _t;
+}
+
+double Constellation::energy() {
+	return _E;
+}
+
+double Constellation::calcE(){
+	return 0;
+}
+
+vector<phaseVec> driverFunc(double t, vector<Body> y) {
+	vector<phaseVec> output(y.size());
+	for (size_t i=0; i!=y.size(); ++i) {
+		Vec v_punt(0, 0); //todo add driver function
+		output.push_back(phaseVec());
+
+
+	}
+	return output;
+}
+
+void Constellation::RK4update(const double h) {
+	vector<phaseVec> k_1 = h * driverFunc(_t, _y);
+	vector<phaseVec> k_2 = h * driverFunc(_t + (h/2), _y + (k_1/2) );
+	vector<phaseVec> k_3 = h * driverFunc(_t + (h/2), _y + (k_2/2) );
+	vector<phaseVec> k_4 = h * driverFunc(_t + h, _y + k_3);
+	_y = _y + k_1/6 + k_2/3 + k_3/3 + k_4/6;
+	_t += h;
+}
+
+void Constellation::print() {
+	cout << _t << ' ' << _E << ' ' << endl;
 }
--- program/vector.h
+++ program/vector.h
@@ -1,13 +1,28 @@
+//==============================================================================
+//
+//header file for all necessary data types for project
+//
+//==============================================================================
+
+
+//safeguard includes
+#ifndef VECTOR_H
+#define VECTOR_H
+
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-#ifndef VECTOR_H
-#define VECTOR_H
-#endif
+//==============================================================================
+//
+//create class for 3D vectors with operators and assignment operators
+//
+//==============================================================================
+
 
 class Vec {
 	double _x;
@@ -18,21 +33,23 @@ public:
 
 //constructor
 	Vec() { _x = 0; _y = 0; _z = 0; }
+	Vec(double x, double y) { _x = x; _y = y; _z = 0; }
 	Vec(double x, double y, double z) { _x = x;  _y = y; _z = z; }
+
 
 //getter
 	double x() const { return _x; }
 	double y() const { return _y; }
 	double z() const { return _z; }
 
-//define norm
+//declare norm
 	double r() const;
 	double r2() const;
-  
- //define inproduct
+
+//declare inproduct
 	double mult(Vec u) const;
 
-//define assignment operators
+//declare assignment operators
 	Vec& operator+=(Vec v);
 	Vec& operator-=(Vec v);
 	Vec& operator*=(double s);
@@ -41,19 +58,141 @@ public:
 };
 
 
-//define vector/vector operators
+//declare vector/vector operators
 Vec operator+(Vec a, Vec b);
 Vec operator-(Vec a, Vec b);
 Vec operator*(Vec a, Vec b);
 
-//define vector/scalar operators
+//declare vector/scalar operators
 Vec operator*(Vec a, double s);
 Vec operator*(double s, Vec a);
 Vec operator/(double s, Vec a);
 Vec operator/(Vec a, double s);
 
-//function for printing vectors
+//declare function for printing vectors
 void print(Vec a);
 
-//function for printing scalars
+//==============================================================================
+
+//declare function for printing scalars
 void print(double s);
+
+//==============================================================================
+//
+//create class for 6D vectors containing position and velocity
+//
+//==============================================================================
+
+class phaseVec {
+//class that represents a 2*3D vector (position/velocity)
+	Vec _x;
+	Vec _v;
+
+public:
+
+//constructor
+	phaseVec() { _x = Vec(); _v = Vec(); }
+	phaseVec(Vec pos, Vec vel) { _x = pos; _v = vel; }
+
+//getter
+	Vec pos() const;
+	Vec vel() const;
+	double x() const;
+	double y() const;
+	double z() const;
+	double v_x() const;
+	double v_y() const;
+	double v_z() const;
+
+//declare assignment operators
+	phaseVec& operator+=(phaseVec v);
+	phaseVec& operator-=(phaseVec v);
+	phaseVec& operator*=(double s);
+	phaseVec& operator/=(double s);
+};
+
+//declare phasevector/phasevector operators
+phaseVec operator+(phaseVec a, phaseVec b);
+phaseVec operator-(phaseVec a, phaseVec b);
+
+//==============================================================================
+//
+//create class for a body in the system
+//
+//==============================================================================
+
+class Body {
+//class that represents a body with a position _x, velocity _v and mass _m
+	Vec _x;
+	Vec _v;
+	double _m;
+
+public:
+
+//constructor
+	Body() { _x = Vec(); _v = Vec(); _m = 1; }
+	Body(double mass) { _x = Vec(); _v = Vec(); _m = mass; }
+	Body(Vec pos, Vec vel, double mass) { _x = pos; _v = vel; _m = mass; }
+	Body(double x, double y, double z, double v_x, double v_y, double v_z, double m);
+
+
+//getter
+	Vec pos() const { return _x; }
+	Vec vel() const { return _v; }
+	double m() const { return _m; }
+
+//declare body/phaseVec operators
+	Body& operator+=(phaseVec b);
+	Body& operator-=(phaseVec b);
+	Body& operator/=(double s);
+	Body& operator*=(double s);
+
+};
+
+//==============================================================================
+//
+//create operations for vector<phaseVec> adding/multiplication/etc
+//create operations for vector<Body> adding
+//
+//==============================================================================
+
+vector<Body> operator+(vector<Body> a, vector<phaseVec> b);
+vector<Body> operator-(vector<Body> a, vector<phaseVec> b);
+vector<phaseVec> operator*(vector<phaseVec> a, double s);
+vector<phaseVec> operator*(double s, vector<phaseVec> a);
+vector<phaseVec> operator/(vector<phaseVec> a, double s);
+
+//==============================================================================
+//
+//create class for a constellation of (variable) N bodys
+//
+//==============================================================================
+
+class Constellation {
+	double _t;
+	double _E;
+	vector<Body> _y;
+
+public:
+
+//constructor
+	Constellation();
+	Constellation(vector<Body> y);
+
+//getter
+	Body particle(size_t n);
+	double time();
+	double energy();
+
+//evolve system
+	void RK4update(double h);
+
+
+//calculate energy
+	double calcE();
+
+//print state
+	void print();
+};
+
+#endif
