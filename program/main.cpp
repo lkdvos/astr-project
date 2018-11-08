6f22d7a4a7d312d38a8605930ae2f52ec59454f2
DevosLukas <Lukas.Devos@uGent.be>
2018-11-08 17:17:53 +0100

    add some testing and first visualisation

--- program/main.cpp
+++ program/main.cpp
@@ -9,17 +9,40 @@ using namespace std;
 
 
 int main() {
+//working in standard units: G = M = 1
+  /*double sunMass = 332946 / (332946 + 1);
+  double earthMass = 1 / (332946 + 1);*/
 
+  double sunMass = 1000000;
+  double earthMass = 1;
+  double h = 0.1;
+  size_t steps = 10000;
 
 //create first body
-  Body sun(1, 0, 0, 0, 0.5, 0, 1);
-  Body earth(-1, 0, 0, 0, -0.5, 0, 1);
-  vector<Body> y = {sun, earth};
-  Constellation a(y);
-  for (size_t i=0; i != 100; ++i) {
-      a.print();
-      a.RK4update(0.1);
-  }
+  Body sun(0, 0, 0, 0, 0, 0, sunMass);
+  cout << "sun " << sun << endl;
+//create second body
+  Body earth(-100, 0, 0, 0, 1, 0, earthMass);
+  cout << "earth "<< earth << endl;
+//create third body
+  Body moon(0, 100, 0, 0, 0, -1, 1);
+//combine bodies
+  vector<Body> y = {sun, earth, moon};
 
+
+  Constellation a(y);
+  string outfile = "data/data.txt";
+  ofstream f(outfile);
+  f.open("data/data.txt", ios::trunc);
+  f << "#{tijd} #{positie1} #{snelheid1} #{...} \n";
+  f.close();
+
+  for (size_t i=0; i!=steps; ++i) {
+    if (i%100 == 0) {
+      a.printFile(outfile);
+    }
+    a.RK4update(h);
+  }
+  a.printFile(outfile);
 
 }
