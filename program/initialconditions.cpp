64547a17f0161f3243928e33d308bd33bcf5facc
MichielVieijra <michiel.vieijra@hotmail.com>
2018-11-17 23:18:32 +0100

    Initial Conditions

---
+++ program/initialconditions.cpp
@@ -0,0 +1,56 @@
+//==============================================================================
+//
+//.cpp file that can be used to read out initial conditions for
+//a certain N-body simulation.
+//
+//==============================================================================
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+#include "vector.h"
+#include "initialconditions.h"
+using namespace std;
+
+// Function that reads out initial conditions for a certain N-body simulation 
+// (contained in a file) and returns a vector containing the particles of the simulation
+vector<Body> initialisation(string filename)
+	{
+	ifstream file_input(filename);
+	// Initialise vector of particles
+	vector<Body> bodies;
+	
+	//==============================================================================
+	//
+	// Initialise mass, velocity components and position components for particles
+	//
+	//==============================================================================
+	
+	double x = 0;
+	double y = 0;
+	double z = 0;
+	double vx = 0;
+	double vy = 0;
+	double vz = 0;
+	double m = 0;
+
+	// The number of particles in the simulation is defined as numpart
+	int numpart = 0;
+	file_input >> numpart;
+	for (int i=0; i<numpart; i++)	// For-loop that reads out lines of file line by line
+		{
+		file_input >> x >> y >> z >> vx >> vy >> vz >> m;
+		Body body = Body(x, y, z, vx, vy, vz, m);
+		// Add each particle to the vector of particles
+		bodies.push_back(body);
+		}
+	return bodies;
+	}
+
+
+
+
+
---
+++ program/initialconditions.h
@@ -0,0 +1,22 @@
+//==============================================================================
+//
+//header file for initialconditions.cpp
+//
+//==============================================================================
+
+#ifndef INITIALCONDITIONS_H
+#define INITIALCONDITIONS_H
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include "vector.h" 
+using namespace std;
+
+
+vector<Body> initialisation(string filename);
+
+#endif
