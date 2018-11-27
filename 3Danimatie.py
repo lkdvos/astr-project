130e3acccdf7644a6ff76d46dbcd3e4444179790
DevosLukas <Lukas.Devos@uGent.be>
2018-11-27 13:21:05 +0100

    create domain, change naming

--- 3Danimatie.py
+++ 3Danimatie.py
@@ -5,18 +5,23 @@ Created on Wed Nov 14 22:39:28 2018
 @author: Iris
 """
 
-#tijd x1 y1 z1 vx1 vy1 vz1 x2... xn yn zn vxn vyn vzn  
+filename = "SunEarth"
+
+#tijd x1 y1 z1 vx1 vy1 vz1 x2... xn yn zn vxn vyn vzn
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
-
-invoer = np.loadtxt("data.txt")
+invoer = np.loadtxt("program/data/{}.txt".format(filename))
 n = int((len(invoer[0])-1)/6) #aantal deeltjes
 
 #omvormen naar lijst van deze vorm:
 #[ [[x1,y1,z1][x2,y2,z2]...[xn,yn,zn]](t0)
 #     [[x1,y1,y2][x2,y2,z2]...[xn,yn,zn]](t1)   ... (tn)]
+
+
+
+
 punten = []
 tijd = []
 for lijn in invoer:
@@ -26,16 +31,41 @@ for lijn in invoer:
         deel.append([lijn[1+i*6], lijn[2+i*6], lijn[3+i*6]])
     punten.append(deel)
 
+#grenzen vinden
+xvals = []
+yvals = []
+zvals = []
+for tijdpunt in punten:
+    for deeltje in tijdpunt:
+        xvals.append(deeltje[0])
+        yvals.append(deeltje[1])
+        zvals.append(deeltje[2])
+
+xmin = min(xvals)
+xmax = max(xvals)
+
+ymin = min(yvals)
+ymax = max(yvals)
+
+zmin = min(zvals)
+zmax = max(zvals)
+
+
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.set_xlabel('X')
+ax.set_xlim(xmin, xmax)
 ax.set_ylabel('Y')
+ax.set_ylim(ymin, ymax)
 ax.set_zlabel('Z')
-    
+ax.set_zlim(zmin, zmax)
+
 def update(p, *fargs):
     ax.set_title('t = {}'.format(round(tijd[p])))
     for i in range(n):
-        ax.scatter3D(punten[p][i][0], punten[p][i][1], punten[p][i][2]) 
+        ax.scatter3D(punten[p][i][0], punten[p][i][1], punten[p][i][2])
 
 ani = animation.FuncAnimation(fig, update, frames = len(punten), fargs=(10,), interval = 100, blit = False)
-ani.save('ani2.gif')
+plt.show()
+ani.save("program/data/{}.mp4".format(filename), writer='ffmpeg')
