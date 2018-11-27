5a1c7c055493146741adb9a06438dc4f51ade53c
DevosLukas <Lukas.Devos@uGent.be>
2018-11-27 22:04:08 +0100

    group Energiefouten

--- energiePlot.py
+++ energiePlot.py
@@ -1,21 +1,48 @@
-filename = "SunEarthL1"
 import numpy as np
 import matplotlib.pyplot as plt
 
-t, E = np.loadtxt("program/data/{}_Energy.txt".format(filename), unpack=True, skiprows=1)
+filenames = [
+"SunEarth",
+"SunEarthL1",
+"SunEarthL1L2",
+]
 
-#grenzen vinden
-Emin = min(E)
-tmin = min(t)
+methods = [
+"RK4",
+"Verlet",
 
-Emax = max(E)
-tmax = max(t)
+]
 
-print(tmin, tmax)
-print(Emin, Emax)
 
-plt.plot()
-plt.semilogy(t, E)
-plt.title('relatieve Efout ifv t')
 
-plt.show()
+def importData(filename, method):
+    t, E = np.loadtxt("program/data/{}{}_Energy.txt".format(filename, method), unpack=True, skiprows=1)
+    Emin = min(E)
+    tmin = min(t)
+
+    Emax = max(E)
+    tmax = max(t)
+    print("imported {} with {}".format(filename, method))
+    print("boundaries are: ")
+    print("t [{}, {}]".format(tmin, tmax))
+    print("E [{}, {}]".format(Emin, Emax))
+    return (t, E, (tmin, tmax), (Emin, Emax))
+
+def plot(filename, methods):
+
+
+    plt.plot()
+    subplots = len(methods)
+
+    for i in range(subplots):
+        t, E, tBounds, Ebounds = importData(filename, methods[i])
+        if i == 0:
+            ding = plt.subplot(1, subplots, i+1)
+        else:
+            plt.subplot(1, subplots, i+1, sharex=ding, sharey=ding)
+        plt.semilogy(t, E)
+        plt.title('{} met {}'.format(filename, methods[i]))
+    plt.show()
+
+for filename in filenames:
+    plot(filename, methods)
