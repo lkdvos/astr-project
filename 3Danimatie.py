9adac9da10fe4ae53be7cb97209537979d1ae414
DevosLukas <Lukas.Devos@uGent.be>
2018-12-02 17:58:27 +0100

    update plotter

--- 3Danimatie.py
+++ 3Danimatie.py
@@ -1,85 +1,102 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 14 22:39:28 2018
+#===============================================================================
+#------------------IMPORTS------------------------------------------------------
+#===============================================================================
 
-@author: Iris
-"""
-
-filename = "SunEarth"
-
-#tijd x1 y1 z1 vx1 vy1 vz1 x2... xn yn zn vxn vyn vzn
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
-invoer = np.loadtxt("program/data/{}Verlet.txt".format(filename))
+
+#===============================================================================
+#-------------------------------------------------------------------------------
+#===============================================================================
+
+filename = "SolarSystem"
+method = "VER"
+
+
+
+frames=1
+scale = 10
+colors = "plasma"
+
+invoer = np.loadtxt("program/data/{}{}.txt".format(filename, method))
 n = int((len(invoer[0])-1)/6) #aantal deeltjes
 
-#kleuren maken
-colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k', 'w')
-
-#omvormen naar lijst van deze vorm:
-#[ [[x1,y1,z1][x2,y2,z2]...[xn,yn,zn]](t0)
-#     [[x1,y1,y2][x2,y2,z2]...[xn,yn,zn]](t1)   ... (tn)]
-
-print(n)
 
 
-punten = []
-tijd = []
-for lijn in invoer[1:500]:
-    tijd.append(lijn[0])
-    deel = []
-    for i in range(n):
-        deel.append([lijn[1+i*6], lijn[2+i*6], lijn[3+i*6]])
-    punten.append(deel)
-print(punten)
+
+transposedData = np.transpose(invoer)
+N = int((len(transposedData) - 1) / 6)
+print(N)
 
 #grenzen vinden
-xvals = []
-yvals = []
-zvals = []
-for tijdpunt in punten:
-    for deeltje in tijdpunt:
-        xvals.append(deeltje[0])
-        yvals.append(deeltje[1])
-        zvals.append(deeltje[2])
 
-xmin = min(xvals)
-xmax = max(xvals)
+xmins = []
+xmaxs = []
+for i in range(N):
+    xmins.append(np.amin(transposedData[6*i + 1]))
+    xmaxs.append(np.amax(transposedData[6*i + 1]))
+xmin = min(xmins)
+xmax = max(xmaxs)
 
-ymin = min(yvals)
-ymax = max(yvals)
+ymins = []
+ymaxs = []
+for i in range(N):
+    ymins.append(np.amin(transposedData[6*i + 2]))
+    ymaxs.append(np.amax(transposedData[6*i + 2]))
+ymin = min(ymins)
+ymax = max(ymaxs)
 
-zmin = min(zvals)
-zmax = max(zvals)
+zmins = []
+zmaxs = []
+for i in range(N):
+    zmins.append(np.amin(transposedData[6*i+ 3]))
+    zmaxs.append(np.amax(transposedData[6*i + 3]))
+zmin = min(zmins)
+zmax = max(zmaxs)
 
-print(xmin, xmax)
-print(ymin, ymax)
-print(zmin, zmax)
-
-fig = plt.figure()
+fig, ax = plt.subplots()
+xdata, ydata, zdata = [], [], []
 ax = plt.axes(projection='3d')
+
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
+ax.set_facecolor((0,0,0,0.4))
 
-ax.set_xlim(xmin, xmax)
-ax.set_ylim(ymin, ymax)
-ax.set_zlim(zmin, zmax)
+def init():
+    global shown
+    shown = []
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+    ax.set_zlim(zmin, zmax)
 
-
-t = ax.set_title('t = 0')
-temp = [ax.scatter3D(punten[0][i][0], punten[0][i][1], punten[0][i][2]) for i in range(n)]
+    xdata, ydata, zdata = [], [], []
+    for i in range(N):
+        xdata.append(transposedData[6*i + 1][0])
+        ydata.append(transposedData[6*i + 2][0])
+        zdata.append(transposedData[6*i + 3][0])
+    shown.append(ax.scatter3D(xdata, ydata, zdata, s=scale, c=np.linspace(0,1,N), cmap=colors))
+    global t
+    t = ax.text2D(0, 0, 't = 0 days', transform=ax.transAxes)
 
 def update(p, *fargs):
+    global t
+    global shown
+    t.remove()
+    if len(shown) >= frames:
+        shown.pop(0).remove()
 
-    t = ax.set_title('t = {:05.2f}'.format(tijd[p]))
-    for elem in temp:
-        elem.remove()
-    for i in range(n):
-        temp[i] = ax.scatter3D(punten[p][i][0], punten[p][i][1], punten[p][i][2], c=colors[i%len(colors)])
+    xdata, ydata, zdata = [], [], []
+    for i in range(N):
+        xdata.append(transposedData[6*i + 1][p])
+        ydata.append(transposedData[6*i + 2][p])
+        zdata.append(transposedData[6*i + 3][p])
 
-ani = animation.FuncAnimation(fig, update, frames = len(punten), fargs=(10,), interval = 100, blit = False)
-plt.show()
-#ani.save("program/data/{}.mp4".format(filename), writer='ffmpeg')
+    t = ax.text2D(0, 0, 't = {:05.2f} days'.format(transposedData[0][p]), transform=ax.transAxes)
+    shown.append(ax.scatter3D(xdata, ydata, zdata, s=scale, c=np.linspace(0,1,N), cmap=colors))
+
+ani = animation.FuncAnimation(fig, update, frames=len(transposedData[0]), fargs=(10,), interval = 10, blit=False, init_func=init)
+#plt.show()
+ani.save("program/movie/{}{}.mp4".format(filename, method), writer='ffmpeg')
